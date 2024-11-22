23f228d71713e1cb2a3ca01e2f5ac0b4d9e1bc34
sekhubede <msekhubede@gmail.com>
2024-11-22 08:39:30 +0200

    Initial commit

---
+++ WebApps/js/index.js
@@ -0,0 +1,26 @@
+// console.log("Hello World!");
+
+// document.body.insertAdjacentHTML("beforeend", "<p>Hello World! from js</p>");
+
+// const userName = prompt("What is your name?");
+
+// document.body.insertAdjacentHTML(
+//   "beforeend",
+//   `<p>Hello ${userName}! from js</p>`
+// );
+
+// const item1 = prompt("Enter first number");
+// const item2 = prompt("Enter second number");
+// const item3 = prompt("Enter third number");
+
+// const subtotal = AddNumbers(item1, item2, item3);
+
+const subtotal = (item1, item2, item3);
+
+// console.log(`Subtotal: ${subtotal}`);
+
+// function AddNumbers(num1, num2, num3) {
+//   return (
+//     Number.parseFloat(num1) + Number.parseFloat(num2) + Number.parseFloat(num3)
+//   );
+// }
